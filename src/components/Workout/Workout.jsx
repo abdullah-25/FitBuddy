7134fdaa8a59129d62c0a28713633723fea83126
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-07-24 17:00:42 -0400

    add btns

--- src/components/Workout/Workout.jsx
+++ src/components/Workout/Workout.jsx
@@ -1,7 +1,21 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import "./Workout.scss";
+
 export default function Workout() {
   return (
     <div className="workout">
-      <h1>add your workouts!!</h1>
+      <div className="workout__heading">Workout</div>
+      <div className="workout__btns">
+        <Button colorScheme="teal" size="xs">
+          Delete Workout
+        </Button>
+        <Button colorScheme="teal" size="xs">
+          Save Workout
+        </Button>
+        <Button colorScheme="teal" size="xs">
+          Add Exercise
+        </Button>
+      </div>
     </div>
   );
 }
