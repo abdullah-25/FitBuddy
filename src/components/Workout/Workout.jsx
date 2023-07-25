a1598bb51352833b57aabeb3cc3b93960b0f12b3
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-07-25 17:11:51 -0400

    add workout btns, modals and conditional rendering logic for workout page

--- src/components/Workout/Workout.jsx
+++ src/components/Workout/Workout.jsx
@@ -1,21 +1,127 @@
 import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  useControllableProp,
+  useControllableState,
+  useDisclosure,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+import Header from "../../components/Header/Header";
 import "./Workout.scss";
 
 export default function Workout() {
-  return (
-    <div className="workout">
-      <div className="workout__heading">Workout</div>
-      <div className="workout__btns">
-        <Button colorScheme="teal" size="xs">
-          Delete Workout
-        </Button>
-        <Button colorScheme="teal" size="xs">
-          Save Workout
-        </Button>
-        <Button colorScheme="teal" size="xs">
+  const [startWorkoutBtnClick, setstartWorkoutBtnClick] =
+    useControllableState(false);
+  const [nameExcercise, setNameExcercise] = useControllableState("");
+  const handleNameExcercise = (e) => setNameExcercise(e.target.value);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const modal1 = useDisclosure();
+  const modal2 = useDisclosure();
+
+  function SaveWorkout() {
+    setstartWorkoutBtnClick(!startWorkoutBtnClick);
+  }
+  function handleAddExcercise() {
+    return (
+      <>
+        <Button onClick={modal1.onOpen} colorScheme="teal" size="md">
           Add Exercise
         </Button>
+
+        <Modal isOpen={modal1.isOpen} onClose={modal1.onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add Exercise</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody></ModalBody>
+
+            <ModalFooter>
+              <Button
+                colorScheme="teal"
+                mr={3}
+                onClick={() => {
+                  modal2.onOpen();
+                  modal1.onClose();
+                }}
+              >
+                Create New
+              </Button>
+              <Button variant="ghost" onClick={modal1.onClose}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        <Modal isOpen={modal2.isOpen} onClose={modal2.onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Create Exercise</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text mb="8px">Name:</Text>
+              <Input
+                value={nameExcercise}
+                onChange={handleNameExcercise}
+                size="sm"
+              />
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="teal" mr={3} onClick={modal2.onClose}>
+                Cancel
+              </Button>
+              <Button variant="ghost">Save</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="workout">
+        <div className="workout__heading">Workout</div>
+        <div className="workout__btns">
+          {startWorkoutBtnClick ? (
+            <Button
+              onClick={() => {
+                SaveWorkout();
+              }}
+              colorScheme="teal"
+              size="md"
+              variant="outline"
+              className="workout__btns--discard"
+            >
+              Discard
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                SaveWorkout();
+              }}
+              colorScheme="teal"
+              size="md"
+            >
+              Start Workout
+            </Button>
+          )}
+          {startWorkoutBtnClick && handleAddExcercise()}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
