750f19a33b694b0600733600c5eb0026d52de9ed
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-08-03 00:58:34 -0400

    getstarted text chnage

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -7,7 +7,7 @@ export default function GetStarted() {
   return (
     <div className="container">
       <img src={logo} alt="logo" className="container__img" />
-      <h1>Welcome to My Awesome App</h1>
+      <h1>Welcome to FitBuddy</h1>
       <p>Start your journey now!</p>
       <Link to="/signup">
         <Button colorScheme="teal" variant="solid" mt={10} mr={2}>
