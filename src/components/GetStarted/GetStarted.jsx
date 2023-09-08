75fbcd9538087916cf5ef8880c606e47ecb7c597
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-09-08 16:27:33 -0400

    redesigned home page

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,24 +1,54 @@
 import logo from "../../assets/logo/logo.png";
 import "../../components/GetStarted/GetStarted.scss";
-import { Button } from "@chakra-ui/react";
+import { Button, Stack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 export default function GetStarted() {
   return (
     <div className="container">
-      <img src={logo} alt="logo" className="container__img" />
-      <h1>Welcome to FitBuddy</h1>
-      <p>Keep Track of Your Gym's Progress</p>
-      <Link to="/signup">
-        <Button colorScheme="teal" variant="solid" mt={10} mr={2}>
-          Get Started
-        </Button>
-      </Link>
-      <Link to="/signin">
-        <Button colorScheme="teal" variant="outline" mt={10} mr={3}>
-          Log In
-        </Button>
-      </Link>
+      <div className="center-content">
+        <div className="container__img">
+          <img src={logo} alt="logo" className="container__img" />
+        </div>
+        <h1>Welcome to FitBuddy</h1>
+        <p className="p">Keep Track of Your Gym's Progress</p>
+      </div>
+      <div className="container__btns">
+        <div className="btn-wrapper">
+          <Link to="/signup">
+            <Button
+              loadingText="Submitting"
+              size="lg"
+              bg={"teal.400"}
+              color={"white"}
+              _hover={{
+                bg: "teal.500",
+              }}
+              width={"300px"}
+              className="container__btns--btn"
+              mb={5}
+            >
+              Sign Up
+            </Button>
+          </Link>
+          <Link to="/signin">
+            <Button
+              loadingText="Submitting"
+              size="lg"
+              bg={"teal.400"}
+              color={"white"}
+              _hover={{
+                bg: "teal.500",
+              }}
+              width={"300px"}
+              className="container__btns--btn"
+              mb={10}
+            >
+              Sign In
+            </Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
